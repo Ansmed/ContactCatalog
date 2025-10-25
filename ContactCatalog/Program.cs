5d651f27282225597a82b2077b2ec60d4b29c701
anton <anton@WIN-948NOUM5K65>
2025-10-25 14:25:42 -0700

    Add project files.

---
+++ ContactCatalog/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactCatalog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
